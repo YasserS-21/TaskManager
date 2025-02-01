a06043203394109a15a06a7635fd24508d8c8480
YasserS <56772311+YasserS-21@users.noreply.github.com>
2025-02-01 03:22:39 -0500

    build: build: add User model entity with JPA annotations

---
+++ src/main/java/model/User.java
@@ -0,0 +1,62 @@
+package model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    // This is the default constructor (Which is required by JPA)
+    public User() {}
+
+    //This is the constructor for creating new users
+    public User(String username, String email, String password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+
+    }
+
+    //Getters and setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
